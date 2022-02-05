5597d68a1a4b2454d8fb308ef6dec7f2424aadbe
Cian Ormond <cow57xd@gmail.com>
2022-02-05 14:08:45 -0800

    Add license info and help page

--- main.cpp
+++ main.cpp
@@ -5,15 +5,40 @@ using namespace std;
 
 char* version = "0.0.1";
 
+int downloadXpilot() {
+
+
+
+    return 0;
+}
+
 //TODO return something for error checking
-void install(char* program) {
+int install(char* program) {
     cout << "Installing " << program << endl;
+    if (strcmp(program, "xpilot") == 0) {
+        downloadXpilot();
+    }
+    return 0;
+}
+
+void showLicense() {
+    cout << "This project is not endorsed by any VATSIM staff member." << endl;
+    cout << "The xPilot 2.0.0 beta is licensed under GPL3. Redistribution is allowed under this license and express " <<
+    "permission was given by Justin Shannon." << endl << endl;
+    cout << "libCURL 7.81.0 has a custom license. Usage is allowed under this license." << endl << endl;
+}
+
+void showCommands() {
+    cout << "-h - displays this page" << endl;
+    cout << "-l - displays license information" << endl;
+    cout << "-i - installs a pilot/ATC client from the following list - " << endl;
+    cout << "     xPilot 2.0.0 beta (X-Plane 11)" << endl;
 }
 
 int main(int argc, char** argv) {
 
-    cout << "VATSIM Program Manager version " << version << " by Cian Ormond" << endl;
-    cout << "This project is not endorsed by any VATSIM staff member." << endl << endl;
+    cout << endl << "VATSIM Program Manager version " << version << " by Cian Ormond" << endl;
+    cout << "Licensed under GPL3. For licensing of programs included, use -l." << endl << endl;
 
     //TODO remove this if statement bs
     if (argc > 1) {
@@ -21,12 +46,10 @@ int main(int argc, char** argv) {
             if (argc > 2) {
                 install(argv[2]);
             } else {
-                cout << "Please specify a program to install." << endl;
+                cout << "Please specify a program to install. Available options are xpilot." << endl;
             }
-        } else if (strcmp(argv[1], "-h") == 0) {
-            //TODO write command list
-            cout << "Command list is a WIP" << endl;
-        }
+        } else if (strcmp(argv[1], "-h") == 0) {showCommands();}
+        else if (strcmp(argv[1], "-l") == 0) {showLicense();}
     } else {
         cout << "No command specified. Use the -h flag for a command list." << endl;
     }
