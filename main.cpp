59b4079e93dd82a287084280efe8ec9c6c937343
Cian Ormond <cow57xd@gmail.com>
2022-02-05 04:34:11 -0800

    Fix cstring comparison

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -12,17 +13,17 @@ void install(char* program) {
 int main(int argc, char** argv) {
 
     cout << "VATSIM Program Manager version " << version << " by Cian Ormond" << endl;
-    cout << "This project is not endorsed by any VATSIM staff member." << endl;
+    cout << "This project is not endorsed by any VATSIM staff member." << endl << endl;
 
     //TODO remove this if statement bs
-    if (argc > 0) {
-        if (argv[0] == "-i") {
-            if (argc > 1) {
-                install(argv[1]);
+    if (argc > 1) {
+        if (strcmp(argv[1], "-i") == 0) {
+            if (argc > 2) {
+                install(argv[2]);
             } else {
                 cout << "Please specify a program to install." << endl;
             }
-        } else if (argv[0] == "-h") {
+        } else if (strcmp(argv[1], "-h") == 0) {
             //TODO write command list
             cout << "Command list is a WIP" << endl;
         }
